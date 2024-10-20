632db809a6c87947af13f28f10e57ae481b99e62
ycy-ycy <yaocy@pku.edu.cn>
2024-10-20 11:55:49 -0500

    test video

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "library.h"
 
 int main(){
-  setBoundaries(30.0);
-  camera* cam = new camera(-10.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,10.0,1.0);
+  setBoundaries(20.0);
+  camera* cam = new camera(-10.0,-0.0,0.0,-0.2,0.1,0.0,0.0,0.0,0.0,15.0,1.0);
   std::vector<celestialBody*> bodies;
   bodies.push_back(new star(1.0,1.0,1.0,2000.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0));
-  cam->takePhoto(bodies,"test/photo.dat",400,8);
+  for (int i=0;i<121;i++){
+    cam->takePhoto(bodies,"test/" + std::to_string(i) + ".dat",400,8);
+    updateAll(cam, bodies, 1.0/60.0);
+  }
   return 0;
 }
